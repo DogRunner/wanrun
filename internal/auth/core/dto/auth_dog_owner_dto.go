3c595d93205a4efb982cb372dde46afe17c544b8
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2024-11-10 00:39:40 +0900

    delete: trash

--- internal/auth/core/dto/auth_dog_owner_dto.go
+++ internal/auth/core/dto/auth_dog_owner_dto.go
@@ -7,10 +7,3 @@ type AuthDogOwnerReq struct {
 	PhoneNumber       string `json:"phoneNumber"`
 	AuthorizationCode string
 }
-
-type ResAuthDogOwner struct {
-	AuthDogOwnerID uint   `json:"authDogOwnerId"`
-	Name           string `json:"name"`
-	Email          string `json:"email"`
-	Sex            string `json:"sex"`
-}
--- internal/auth/core/dto/dog_owner_dto.go
+++ internal/auth/core/dto/dog_owner_dto.go
@@ -1,10 +1,5 @@
 package dto
 
-// type ResDogOwnerDto struct {
-// 	DogOwnerID uint64
-// 	JwtID      string
-// }
-
 type DogOwnerDTO struct {
 	DogOwnerID uint64
 	JwtID      string
