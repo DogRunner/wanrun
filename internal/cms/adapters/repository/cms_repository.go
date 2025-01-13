0f1a653e9abff147bb914952336fbbcea770eb25
kkkkkkkkazuya <k.kazuya.3380@gmail.com>
2025-01-13 23:15:29 +0900

    create: cms deletefile

--- internal/cms/adapters/repository/cms_repository.go
+++ internal/cms/adapters/repository/cms_repository.go
@@ -10,6 +10,8 @@ import (
 
 type ICmsRepository interface {
 	CreateS3FileInfo(c echo.Context, s3FileInfo model.S3FileInfo) error
+	GetS3FileInfoByFileID(c echo.Context, fileID string) ([]model.S3FileInfo, error)
+	DeleteS3FileInfo(c echo.Context, s3FileInfo model.S3FileInfo) error
 }
 
 type cmsRepository struct {
@@ -41,6 +43,67 @@ func (cr *cmsRepository) CreateS3FileInfo(c echo.Context, s3FileInfo model.S3Fil
 		logger.Error(wrErr)
 		return wrErr
 	}
+
+	return nil
+}
+
+// GetS3FileInfoByFileID: FileIDを元にS3FileInfo取得
+//
+// args:
+//   - echo.Context: Echoのコンテキスト。リクエストやレスポンスにアクセスするために使用
+//   - string: fileID
+//
+// return:
+//   - []model.S3FileInfo: S3ファイル情報
+//   - error: error情報
+func (cr *cmsRepository) GetS3FileInfoByFileID(c echo.Context, fileID string) ([]model.S3FileInfo, error) {
+	logger := log.GetLogger(c).Sugar()
+
+	s3Files := []model.S3FileInfo{}
+	if err := cr.db.Model(&model.S3FileInfo{}).
+		Where("file_id = ?", fileID).
+		Find(&s3Files).Error; err != nil {
+		wrErr := wrErrors.NewWRError(
+			err,
+			"DBからのデータ取得に失敗しました。",
+			wrErrors.NewCmsServerErrorEType(),
+		)
+		logger.Errorf("DB search failure: %v", wrErr)
+
+		return []model.S3FileInfo{}, wrErr
+	}
+
+	logger.Debugf("Query Results: %v", s3Files)
+
+	return s3Files, nil
+}
+
+// DeleteS3FileInfo: S3FileInfoの削除
+//
+// args:
+//   - echo.Context: Echoのコンテキスト。リクエストやレスポンスにアクセスするために使用
+//   - model.S3FileInfo: s3情報
+//
+// return:
+//   - error: error情報
+func (cr *cmsRepository) DeleteS3FileInfo(c echo.Context, s3Info model.S3FileInfo) error {
+	logger := log.GetLogger(c).Sugar()
+
+	if err := cr.db.Model(&model.S3FileInfo{}).
+		Where("file_id = ? AND s3_object_key = ?", s3Info.FileID, s3Info.S3ObjectKey).
+		Delete(&model.S3FileInfo{}).
+		Error; err != nil {
+		wrErr := wrErrors.NewWRError(
+			err,
+			"DBへの削除処理が失敗しました。",
+			wrErrors.NewCmsServerErrorEType(),
+		)
+		logger.Errorf("DB delete failure: %v", wrErr)
+
+		return wrErr
+	}
+
+	logger.Infof("Successfully deleted S3 file info with file_id: %s and s3_object_key: %s", s3Info.FileID.String, s3Info.S3ObjectKey.String)
 
 	return nil
 }
--- migrate/migration_sql/000017_s3_file_info.up.sql
+++ migrate/migration_sql/000017_s3_file_info.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS s3_file_info (
     s3_file_info_id serial primary key,             -- PK
     dog_owner_id INT NOT NULL,                      -- dog_ownersのFK
-    file_id VARCHAR(64) NOT NULL,                   -- 識別用UUID
+    file_id VARCHAR(64) unique NOT NULL,                   -- 識別用UUID
     s3_version_id VARCHAR(256),                     -- S3のバージョンI
     file_size BIGINT NOT NULL,                      -- ファイルサイズ
     s3_object_key varchar(256),                     -- S3のオブジェクトキー
